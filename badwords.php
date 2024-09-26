04130c4c9c009c370ee378bb5f57b793e6cdda58
Antonio Stassi <stassiant@outlook.it>
2024-09-26 19:59:56 +0200

    Excersise completed

--- badwords.php
+++ badwords.php
@@ -1,3 +1,10 @@
+<?php 
+    $phrase = $_GET["phrase"];
+    $word = $_GET["word"];
+    $newPhrase = str_replace($word, 'bobba', $phrase);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +13,10 @@
     <title>Badwords Maramedwords</title>
 </head>
 <body>
+    <p>La frase inviata è la seguente: <?php echo $phrase; ?></p>
+    <p>La sua lunghezza è di: <?php echo strlen($phrase)?> caratteri.</p>
+
+    <p>La frase censurata è la seguente: <?php echo $newPhrase?></p>
     
 </body>
 </html>
--- index.php
+++ index.php
@@ -6,6 +6,19 @@
     <title>BadWords Index</title>
 </head>
 <body>
-    
+    <form action="badwords.php" method="GET">
+        <label for="phrase">Frase</label>
+        <input type="text" name="phrase">
+        <label for="word">Parola da censurare</label>
+        <input type="text" name="word">
+
+        <button type="submit">Ottieni il risultato</button>
+    </form>
 </body>
 </html>
+
+<style>
+    input, button {
+        padding:15px;
+    }
+</style>
